64fd9a8d6b2e10d1d3f53601c6972a4483c84e11
Dilip <dilip.samanta@gmail.com>
2026-01-20 22:48:28 +0530

    fix: run sonarqube check in push as well

--- src/api/server.py
+++ src/api/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello from the Flask Boilerplate Project!"})
+    return jsonify({"message": "Hello from the Flask Boilerplate Project!!"})
 
 
 @app.route("/health")
