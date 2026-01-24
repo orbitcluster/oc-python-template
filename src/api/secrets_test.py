41810d7845d64a41acc945b8a948a40484e0edb1
Dilip <dilip.samanta@gmail.com>
2026-01-24 13:50:39 +0530

    feat: also pushinf secrets test

--- src/api/secrets_test.py
+++ src/api/secrets_test.py
@@ -22,7 +22,7 @@ MIIEowIBAAKCAQEA0Z3VS5JJcds3xfn/ygWyF8PbnGy0AHB7MxszR7TESTKEYx
 
 # Password variables
 password = "VerySecretPassword123!"
-secret_key = "mysupersecretekey_abcdef1634567891"
+secret_key = "mysupersecretekey_abcdef1634567892"
 
 # Auth token with bearer pattern
 auth_token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
